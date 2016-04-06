b45fb9e55a3dd9682c378566a549217a6695410a
G14 <G14@G14s-iMac.local>
2016-04-06 17:27:00 +0500

    remove smooth scroll

--- src/js/common.js
+++ src/js/common.js
@@ -7,16 +7,6 @@ $(function() {
     });
   };
 
-  //Chrome Smooth Scroll
-  try {
-    $.browserSelector();
-    if($("html").hasClass("chrome")) {
-      $.smoothScroll();
-    }
-  } catch(err) {
-
-  };
-
   $("img, a").on("dragstart", function(event) { event.preventDefault(); });
 
 });
