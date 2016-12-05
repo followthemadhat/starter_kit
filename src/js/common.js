5018ed947c542a40eb6ea57c2e41e8bee62ca15f
G14 <G14@G14s-iMac.local>
2016-12-05 21:01:46 +0500

    clear common js

--- src/js/common.js
+++ src/js/common.js
@@ -1,12 +1,3 @@
 $(function() {
-
-  //SVG Fallback
-  if(!Modernizr.svg) {
-    $("img[src*='svg']").attr("src", function() {
-      return $(this).attr("src").replace(".svg", ".png");
-    });
-  };
-
-  $("img, a").on("dragstart", function(event) { event.preventDefault(); });
-
+  
 });
